f9f6ac10912a5554d027ab642c8fb16e625423b5
Alexandre <awello14k@gmail.com>
2018-06-27 22:18:21 +0200

    nodejs + phantomjs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.get('/', function (req, res) {
@@ -7,12 +8,46 @@ app.get('/', function (req, res) {
 
 app.get('/connexion', function(req, res) {
 
-  res.send('Connexion leboncoin avec selenium');
+  res.send('Connexion leboncoin avec selenium version ++');
 
   let email = 'alexandre.sergent.dev@outlook.com'
   let pwd = 'password96'
 
 });
+
+app.get('/snap', function (req, res, next) {
+
+        console.log(req.query.url)
+
+        var savePath = path.join(__dirname, 'public', 'screen') + '.png';
+        var cmd = ['bin/phantomjs_osx', 'generator.js', req.query.url, savePath, 700, 1].join(' ');
+        var exec = require('child_process').exec;
+
+        exec(cmd, function (error) {
+            if (error) {
+                res.status(422);
+                return res.json({ error: error});
+                return res.json({ message: 'Something went wrong, try reloading the page' });
+            }
+
+            return res.json({ path: '/screenshots/screen'+'.png' });
+        });
+
+});
+
+app.get('/screen', function(req, res) {
+
+  var fs = require('fs');
+  fs.readFile( __dirname + '/public/screen.png', function (err, data) {
+    if (err) {
+      throw err;
+    }
+    res.write(data);
+    res.end();
+  });
+
+});
+
 
 app.listen(process.env.PORT || 5000);
 
