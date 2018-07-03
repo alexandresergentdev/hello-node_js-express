5385f4b9b39bcd2a1191ac75ff4dcadd58f523e4
Alexandre <awello14k@gmail.com>
2018-07-03 22:31:02 +0200

    update

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const url = require('url');
 const leboncoin = require('./puppeteer/leboncoin');
 const app = express();
 
+const fs = require('fs');
+
 const log4js = require('log4js');
 log4js.configure({
   appenders: { puppeteer: { type: 'file', filename: 'puppeteer.log' } },
@@ -14,6 +16,13 @@ const logger = log4js.getLogger('puppeteer');
 app.get('/', function(req, res) {
   logger.trace('index');
   res.end('index leboncoin puppeteer with log4js')
+});
+
+app.get('/log', function(req, res) {
+  fs.readFile('puppeteer.log', 'utf8', function(err, data) {
+      if (err) throw err;
+      res.end(data);
+  });
 });
 
 app.get('/connexion/', function(req, res) {
